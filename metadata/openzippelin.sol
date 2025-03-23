a586e8a7ad88600e2d4eb0dc062f738609254b83
George <bookland.commerce@gmail.com>
2025-03-24 01:44:17 +0400

    Update openzippelin.sol

--- metadata/openzippelin.sol
+++ metadata/openzippelin.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT
+
 // Compatible with OpenZeppelin Contracts ^5.0.0
+
+
 pragma solidity ^0.8.22;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -51,4 +54,3 @@ contract LIvetagNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         return super.supportsInterface(interfaceId);
     }
 }
-
